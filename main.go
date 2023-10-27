48e31fc2909c5875dee97a8325fd7fd86fa606cf
aoluwasegun <aoluwasegun@1wa.org>
2023-10-27 13:22:00 +0100

    added static data to vies

--- main.go
+++ main.go
@@ -2,18 +2,37 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+type Person struct {
+	Name string
+	Age uint16
+}
 
-	fmt.Fprintf(w, "Hello, world!\n")
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	person := Person{
+		Name: "Akinduro",
+		Age: 30,
+	}
+
+	generateHTML(w, person, "layout", "navbar", "content")
+}
+
+func generateHTML(w http.ResponseWriter, data interface{}, fn ...string) {
+	var files []string
+	for _, file := range fn {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
 }
 
 func main() {
 	fmt.Println("Hello server!")
 	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir("/public"))
+	files := http.FileServer(http.Dir("public"))
 
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
 	mux.HandleFunc("/", indexHandler)
